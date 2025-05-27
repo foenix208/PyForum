b4bf07b9722159395da875ca4e48dc60d00faa9c
ubuntu <foenix208@gmail.com>
2025-05-27 13:07:32 +0200

    cheplus ToT

--- src/app.py
+++ src/app.py
@@ -1,7 +1,26 @@
-from flask import Flask , render_template
+from flask import Flask , render_template,request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html")    
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def inscription():
+
+    if request.method == "POST":
+        # request.form contains the product data dictionnary
+
+        name = request.form.get("name")
+        price = request.form.get("price")
+        return f"<h1>Product added</h1><p>Name : {name}</p><p>Price : {price}</p>"
+
+    # GET request => show the form
+    return render_template("inscription.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def connection():
+    return render_template("connection.html")
